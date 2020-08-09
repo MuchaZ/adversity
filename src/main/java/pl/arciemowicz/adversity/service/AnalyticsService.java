f9615dc8f9555a01623ff7ec005d77352db78d63
Bartosz Arciemowicz <bartosz@arciemowicz.pl>
2020-08-09 17:05:08 +0200

    Define AnalyticsService interface.

--- src/main/java/pl/arciemowicz/adversity/service/AnalyticsService.java
+++ src/main/java/pl/arciemowicz/adversity/service/AnalyticsService.java
@@ -1,4 +1,14 @@
 package pl.arciemowicz.adversity.service;
 
+import pl.arciemowicz.adversity.controller.AnalyticsCriteria;
+
+import java.time.LocalDate;
+
 public interface AnalyticsService {
+
+    long getTotalClicks(LocalDate dateFrom, LocalDate dateTo, AnalyticsCriteria analyticsCriteria);
+
+    long getCtr(AnalyticsCriteria analyticsCriteria);
+
+    long getImpressionsOverTime(AnalyticsCriteria analyticsCriteria);
 }

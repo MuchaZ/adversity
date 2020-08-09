b30146357e9561b7f40d98f7e3975f8f9e7608cc
Bartosz Arciemowicz <bartosz@arciemowicz.pl>
2020-08-09 16:54:42 +0200

    Redefine contract.

--- src/main/java/pl/arciemowicz/adversity/controller/AnalyticsController.java
+++ src/main/java/pl/arciemowicz/adversity/controller/AnalyticsController.java
@@ -1,14 +1,32 @@
 package pl.arciemowicz.adversity.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.arciemowicz.adversity.domain.AnalyticsData;
+
+import java.time.LocalDate;
 
 @RestController("/api/analytics")
 public class AnalyticsController {
 
     @GetMapping("/data")
     public AnalyticsData analyticsData(AnalyticsCriteria analyticsCriteria) {
+        return new AnalyticsData();
+    }
+
+    @GetMapping("/data/totalClicks")
+    public AnalyticsData totalClicks(@RequestParam LocalDate dateFrom, @RequestParam LocalDate dateTo, @RequestParam String dataSource, AnalyticsCriteria analyticsCriteria) {
+        return new AnalyticsData();
+    }
+
+    @GetMapping("/data/ctr")
+    public AnalyticsData ctr(AnalyticsCriteria analyticsCriteria) {
+        return new AnalyticsData();
+    }
+
+    @GetMapping("/data/impressions")
+    public AnalyticsData impressions(AnalyticsCriteria analyticsCriteria) {
         return new AnalyticsData();
     }
 }

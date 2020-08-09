26fd3fb24a7761ceb9dac0be91442e9ddc807fa8
Bartosz Arciemowicz <bartosz@arciemowicz.pl>
2020-08-09 16:31:11 +0200

    Define AnalyticsController's endpoint for analytics data.

--- src/main/java/pl/arciemowicz/adversity/controller/AnalyticsController.java
+++ src/main/java/pl/arciemowicz/adversity/controller/AnalyticsController.java
@@ -1,4 +1,14 @@
 package pl.arciemowicz.adversity.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.arciemowicz.adversity.domain.AnalyticsData;
+
+@RestController("/api/analytics")
 public class AnalyticsController {
+
+    @GetMapping("/data")
+    public AnalyticsData analyticsData() {
+        return new AnalyticsData();
+    }
 }
